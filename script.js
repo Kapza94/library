cb4f10ba8edbab13c89fe4021be9aafbf88d1e74
Kapza <lukakapetanovic@icloud.com>
2024-11-29 13:35:31 +0100

    map function showingbooks on the screen

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const myLibrary = [];
 
+const bookContainer = document.getElementById("book-container");
+
 function book(name, author) {
   (this.name = name), (this.author = author);
 }
 
-function addBook() {
-  let newBook = new book("Harry Potter- The prisoner of Azkaban", "J.K.Rowling");
+function addBook(title, author) {
+  let newBook = new book(title, author);
   myLibrary.push(newBook);
 }
+addBook("Harry Potter and the philosophers stone", "J.K.Rowling");
+addBook("Harry Potter Prisoner of Azkaban", "J.K.Rowling");
 
-console.log(addBook(), myLibrary);
+myLibrary.map((book) => {
+  const newBook = document.createElement("li");
+  newBook.textContent = `${book.name} ${book.author}`;
+  newBook.setAttribute("id", `${book.name}`);
+  bookContainer.appendChild(newBook);
+  console.log(newBook);
+});
+
 // console.log(myLibrary);
